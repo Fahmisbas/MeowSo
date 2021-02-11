aa684d3e49d7ca781a1f4722c52c72545bc5d260
Fahmi <fahmisulaimanbas@gmail.com>
2021-02-11 17:11:06 +0700

    refactor : adding user attributes

--- app/src/main/java/com/revelatestudio/meowso/data/dataholder/auth/LoggedInUser.kt
+++ app/src/main/java/com/revelatestudio/meowso/data/dataholder/auth/LoggedInUser.kt
@@ -1,12 +1,26 @@
 package com.revelatestudio.meowso.data.dataholder.auth
 
-import android.net.Uri
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
 
+@Parcelize
 data class LoggedInUser(
-    val userId: String,
-    val displayName: String,
-    val userName: String,
-    val email: String,
-    val photoUrl: Uri,
-)
+    val uid: String,
+    val displayName: String?,
+    val userName: String?,
+    val email: String?,
+    val photoUrl: String?,
+    val profileDescription: String? = "",
+    val followingCount: String? = INITIAL_COUNT_VALUE,
+    val followersCount: String? = INITIAL_COUNT_VALUE,
+    val postsCount: String? = INITIAL_COUNT_VALUE
+) : Parcelable {
+
+    companion object {
+        private const val INITIAL_COUNT_VALUE = "0"
+    }
+
+}
+
+
